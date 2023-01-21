62e1fec771c5944084aecf8582e99f10c0075c8c
Philip Bertani <philip.bertani@gmail.com>
2023-01-20 22:19:07 -0500

    more changes for render deploy

--- server/db/db.js
+++ server/db/db.js
@@ -22,4 +22,8 @@ if(process.env.DATABASE_URL){
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config)
+
+  //postgres://play:vUnTBCRyYR7kW5FMVc8nW1gts0cqeTzx@dpg-cf5lalla499d72s5g9bg-a/mancala
+  //postgres://play:vUnTBCRyYR7kW5FMVc8nW1gts0cqeTzx@dpg-cf5lalla499d72s5g9bg-a.ohio-postgres.render.com/mancala
+
 module.exports = db
